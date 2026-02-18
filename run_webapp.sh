3fab7ef3770f9cc5349e97b87c7b5845ffcd725f
Janzell <jjurilla@vwise.com>
2026-02-18 21:17:49 +0800

    fix: generated images

--- run_webapp.sh
+++ run_webapp.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-# Script to activate venv and run the Flask app with pipenv
-cd /Users/janzell/Documents/projects/antscorner
-source venv/bin/activate
+# Script to run the Flask app with pipenv
+cd /Users/janzell/Documents/projects/ants-corner-barcode
 pipenv run python app.py
